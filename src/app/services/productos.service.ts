2bac4ddd53bcfc1af808521a77c114ccb99789ca
guillo <guillermo.trespalacios@gmail.com>
2018-01-30 16:38:40 -0500

    Primera versión

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -6,6 +6,7 @@ export class ProductosService {
 
   productos:any[] = [];
   cargando:boolean = true;
+  productos_filtrado:any[] = [];
 
   constructor( private http:Http ) {
 
@@ -13,13 +14,58 @@ export class ProductosService {
 
   }
 
+
+
+  public buscar_producto(termino: string){ 
+
+
+    if(this.productos.length === 0){
+      this.cargar_productos().then(()=>{
+        // Termino la carga 
+        this.filtrar_productos(termino);
+        // console.log('Buscando producto');
+        // console.log(this.productos_filtrado.length);
+      });
+    }
+    else{
+      this.filtrar_productos(termino);
+      // console.log('Buscando producto');
+      // console.log(this.productos_filtrado.length);
+    }
+  
+  }
+
+
+
+  private filtrar_productos(termino:string){
+    
+    this.productos_filtrado =  [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+      if(prod.categoria.toLowerCase().indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >=0 ){
+        this.productos_filtrado.push(prod);
+        // console.log(prod);
+      }
+    });
+
+  }
+
+
+
   public cargar_productos(){
     this.cargando = true;
-    this.http.get("https://paginawebguillo.firebaseio.com/productos_idx.json").subscribe(
-      respuesta => { 
-        this.cargando = false;
-        this.productos = respuesta.json();
-      });
+    let promesa = new Promise((resolve, reject) => {
+
+      this.http.get("https://paginawebguillo.firebaseio.com/productos_idx.json").subscribe(
+        respuesta => { 
+          this.cargando = false;
+          this.productos = respuesta.json();
+          resolve();
+        });
+    
+    });
+    return promesa;
   }
 
 
